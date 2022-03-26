89266a94d99f3996ab50623f54f01c5019e05831
Gabriel Albernaz <gabrielalbernaz22@gmail.com>
2022-03-26 02:06:31 -0300

    Fix bugs

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -2,13 +2,21 @@ export default function initMobile() {
     const hamburgerIcon = document.querySelector(".bars")
     const navbar = document.querySelector("nav.navbar")
     const navElements = document.querySelectorAll("[data-mobile]")
+    const arrowDownIcon = document.querySelector(".arrow-down")
 
     function mobileMenuOpen() {
         navElements.forEach((element) => {
             const elementCls = element.dataset.mobile
             element.classList.toggle(elementCls)
         })
+
+        if(navbar.classList.contains("open")) {
+            arrowDownIcon.classList.add("hide")
+        } else {
+            arrowDownIcon.classList.remove("hide")
+        }
     }
+    
 
     hamburgerIcon.addEventListener("click", mobileMenuOpen)
 }
